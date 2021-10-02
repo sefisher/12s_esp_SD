0a4b002347b2e59b6af2f242abc639438462d724
GhostDaddio <burkemcs-github@yahoo.com>
2021-10-02 13:28:52 -0400

    Map pins and settings to 8-21-21 board design

--- src/config.cpp
+++ src/config.cpp
@@ -16,11 +16,11 @@ i2s_config_t i2s_adc_config = {
 
 // i2s config for reading from I2S
 i2s_config_t i2s_mic_Config = {
-    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
+    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_PDM),
     .sample_rate = SAMPLE_RATE,
     .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
-    .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
-    .communication_format = I2S_COMM_FORMAT_I2S,
+    .channel_format = I2S_MIC_CHANNEL, 
+    .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB), 
     .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
     .dma_buf_count = 4,
     .dma_buf_len = 1024,
